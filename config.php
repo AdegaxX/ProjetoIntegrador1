25e6f23ba30702190640771379015f7249f32c2d
Leandro Adegas <ecommerce.adegas@gmail.com>
2024-11-22 20:49:59 -0300

    alt

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'usuarios';
+    $dbHost = ' containers-us-west-XXXXXX.railway.app'; // Host do banco de dados no Railway
+    $dbUsername = 'railway'; // Usuário do banco
+    $dbPassword = 'senha'; // Senha do banco
+    $dbName = 'nome_do_banco'; // Nome do banco
+    $dbPort = 'XXXXX'; // Porta do banco
 
-    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+    $conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
 
     
 
