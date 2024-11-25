5aaf93c453df559178f0a9186d4aee9d50b1b716
Leandro Adegas <94626997+AdegaxX@users.noreply.github.com>
2024-11-25 14:01:13 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Pegue as informações do banco de dados pelas variáveis de ambiente
-$dbHost = 'mysql.railway.internal'; // Altere se necessário
+$dbHost = '/var/lib/mysql'; // Altere se necessário
 $dbUsername = 'root';
 $dbPassword = 'KTgoLggUEvpjZrBNcoVJzDWeTmcQqVIf';
 $dbName = 'Repositorio';
